dec2ebb75c0a838024448d4351fd925ba7058468
arshiacomplus <arshiacomplus@gmail.com>
2026-05-04 13:40:19 +0330

    New version 33 / fiexed PYROGRAM FLOOD

--- core/tg_downloader.py
+++ core/tg_downloader.py
@@ -2,6 +2,22 @@ import os
 from pyrogram import Client
 from core.progress import ProgressUpdater
 
+
+pyro_client = None
+
+async def get_client(api_id, api_hash, bot_token):
+    global pyro_client
+    if pyro_client is None:
+
+        pyro_client = Client(
+            name="rgit_pyrogram",
+            api_id=api_id,
+            api_hash=api_hash,
+            bot_token=bot_token
+        )
+        await pyro_client.start()
+    return pyro_client
+
 async def download_large_tg_file(
     api_id: int,
     api_hash: str,
@@ -12,20 +28,16 @@ async def download_large_tg_file(
     updater: ProgressUpdater
 ) -> str:
 
-    async with Client(
-        name="rgit_bot_session",
-        api_id=api_id,
-        api_hash=api_hash,
-        bot_token=bot_token,
-        in_memory=True
-    ) as app:
-        msg = await app.get_messages(chat_id, message_id)
 
-        def progress(current, total):
-            if total:
-                percent = (current / total) * 100
-                updater.update_sync(percent, f"{current//1024//1024}MB", "Calc...")
+    app = await get_client(api_id, api_hash, bot_token)
 
-        await app.download_media(msg, file_name=dest_path, progress=progress)
+    msg = await app.get_messages(chat_id, message_id)
+
+    def progress(current, total):
+        if total:
+            percent = (current / total) * 100
+            updater.update_sync(percent, f"{current//1024//1024}MB", "Calc...")
+
+    await app.download_media(msg, file_name=dest_path, progress=progress)
 
     return dest_path
